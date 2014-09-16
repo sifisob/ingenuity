09c94b2aab7bedcf51590796df7ef156555e0635
sifisob <stillpledged@gmail.com>
2014-09-16 22:04:35 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ingenuity::Application.config.secret_key_base = '6a81eec107124bc744cfaf72819001e846f1888a67ae82e6a46c76a68b60cfe25ea9fdf60971955812dbe1e82757a934e1b2d0d68efa0bef86b916e768a60d2f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ingenuity::Application.config.session_store :cookie_store, key: '_ingenuity_session'
